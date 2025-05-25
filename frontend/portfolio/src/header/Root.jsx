27a972076871a1ab95d60f7274406240ebd264bb
Samy Mizi Allaoua <2393410@cmontmorency.qc.ca>
2025-05-25 10:25:08 -0400

    Changement de répértoire

---
+++ frontend/portfolio/src/header/Root.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+
+const Root = () => {
+    return (
+        <>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+        </>
+    )
+}
+export default Root;

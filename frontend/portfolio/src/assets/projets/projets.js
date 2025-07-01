03cd6dff024f3bf1f85b2b5a9318a006ecf25c8a
Samy Mizi Allaoua <2393410@cmontmorency.qc.ca>
2025-07-01 13:54:09 -0400

    Ajustement des projets et photo personnelleaccueil

--- frontend/portfolio/src/assets/projets/projets.js
+++ frontend/portfolio/src/assets/projets/projets.js
@@ -1,22 +1,3 @@
-import roblox from "./roblox.jpg";
-import python from "./python.png";
 
-const projects = [
-    {
-        id:1,
-        image:roblox,
-        name:"Serveur Roblox",
-        language:"Lua",
-        description:"Un serveur de click to win pour améliorer mes abilités en jexu vidéo et a m'adapter a d'autre langage de programmation comme le Lua et le framwoek API de Rblox Studio.",
-        link:"test",
-    },
-    {
-        id:2,
-        image:python,
-        name:"Logiciel de physique",
-        language:"Python",
-        description:"Juste un logiciel en python pour tester mes connaissance et apprendre a instaurer un objet en 3D et le faire bouger dans un espace 3D.",
-        link:"test",
-    },
-]
+const projects = []
 export default projects;
--- frontend/portfolio/src/home/Home.jsx
+++ frontend/portfolio/src/home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     return (
         <>
         <div id="home1">
-            <img src={samy} alt="" height={550} width={400}/>
+            <img src="" alt="" height={550} width={400}/>
             <h1>Bienvenu sur mon portfolio</h1>
             <p id="p1">Salut, je m’appele Samy, j’ai 19 ans et j’étudie en informatique au cegept Montmorency depuis maintenants deux ans. </p>
             <p id="p2">Ma passion pour l’informatique a commencé dès l’instant où j’ai touché un ordinateur, et elle s’est intensifiée lorsque j’ai assemblé mon tout premier PC. Qu’il s’agisse de la création de jeux vidéo, de sites web ou d’applications, chaque projet alimente ma soif d’apprendre. Mon prochain objectif est de poursuivre mes études à l’université pour approfondir mes connaissances et réaliser mes ambitions.</p>

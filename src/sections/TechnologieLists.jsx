23e26b3b0441b6134ea8e4718ec373c92201d07f
SamyM-A <samymiziallaoua@hotmail.com>
2025-12-21 12:36:38 -0500

    Add animation for the liste techno

--- src/sections/TechnologieLists.jsx
+++ src/sections/TechnologieLists.jsx
@@ -1,7 +1,9 @@
 import tech from "../assets/technologies/tech";
 import Technologie from "../components/Technologie";
+import "../App.css";
 
 const TechnologieList = () => {
+  
   const langue = tech
     .filter((t) => t.type === "langage")
     .map((t) => <Technologie key={t.id} img={t.img} name={t.name} />);
@@ -9,17 +11,34 @@ const TechnologieList = () => {
     .filter((t) => t.type === "framework")
     .map((t) => <Technologie key={t.id} img={t.img} name={t.name} />);
   const autre = tech
-    .filter((t) => t.type === "langage")
+    .filter((t) => t.type === "autre")
     .map((t) => <Technologie key={t.id} img={t.img} name={t.name} />);
+
   return (
     <div id="home2">
-      <h1>Mes technologies !</h1>
-      <h3>Langage de programmation</h3>
-      <ul className="ul">{langue}</ul>
-      <h3>Frameworks</h3>
-      <ul className="ul">{frameworks}</ul>
-      <h3>Autre technologie</h3>
-      <ul className="ul">{autre}</ul>
+        <h1>Mes technologies !</h1>
+        
+        <h3>Langage de programmation</h3>
+        <div className="scroller">  
+          <ul className="scroller__inner">{langue}</ul>
+          <ul className="scroller__inner">{langue}</ul>
+          <ul className="scroller__inner">{langue}</ul>
+        </div>
+
+        <h3>Frameworks</h3>
+          <div className="scroller">  
+          <ul className="scroller__inner">{frameworks}</ul>
+          <ul className="scroller__inner">{frameworks}</ul>
+          <ul className="scroller__inner">{frameworks}</ul>
+          <ul className="scroller__inner">{frameworks}</ul>
+        </div>
+        
+        <h3>Autre technologie</h3>
+          <div className="scroller">  
+          <ul className="scroller__inner">{autre}</ul>
+          <ul className="scroller__inner">{autre}</ul>     
+          <ul className="scroller__inner">{autre}</ul>     
+        </div>
     </div>
   );
 };

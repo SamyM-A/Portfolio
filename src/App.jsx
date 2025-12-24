4022907c923506bd3d89217f064c98fab94d22a4
SamyM-A <samymiziallaoua@hotmail.com>
2025-12-24 08:49:30 -0500

    Add router for header

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,64 @@
-import './App.css'
-import Home from './sections/Home'
-import Header from './sections/Header'
-import Projets from './sections/ProjectsLists'
-import Footer from './sections/Footer'
-import Contact from './sections/Contact'
-import Technologie from './sections/TechnologieLists'
+import { useRef } from "react";
+import './App.css';
+import Home from './sections/Home';
+import Header from './sections/Header';
+import Projets from './sections/ProjectsLists';
+import Footer from './sections/Footer';
+import Contact from './sections/Contact';
+import Technologie from './sections/TechnologieLists';
+import Cv from "./sections/Cv";
 
 function App() {
+
+  const homeRef = useRef(null);
+  const technoRef = useRef(null);
+  const projectsRef = useRef(null);
+  const contactRef = useRef(null);
+  const cvRef = useRef(null);
+
+  const scrollTo = (section) => {
+    const sections = {
+      accueil: homeRef,
+      technologie: technoRef,
+      projects: projectsRef,
+      contact: contactRef,
+      cv: cvRef,
+    };
+
+    sections[section]?.current.scrollIntoView({
+      behavior: "smooth"
+    });
+  };
+
   return (
     <>
-      <Header />
+      <Header scrollTo={scrollTo} />
+
       <main>
-        <Home />
-        <Technologie/>
-        <Projets/>
-        <Contact/>
+        <section ref={homeRef}>
+          <Home />
+        </section>
+
+        <section ref={technoRef}>
+          <Technologie />
+        </section>
+
+        <section ref={projectsRef}>
+          <Projets />
+        </section>
+
+        <section ref={cvRef}>
+          <Cv/>
+        </section>
+
+        <section ref={contactRef}>
+          <Contact />
+        </section>
       </main>
-      <Footer/>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
